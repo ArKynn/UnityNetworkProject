572a07e81c109340efce12e342cb680daa78eec2
David Mendes <david.f.mendes@proton.me>
2024-06-30 13:01:56 +0100

    Added Corpse Behavior

---
+++ Assets/Scripts/Corpse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Corpse : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(StartTimer(5, DestroyThis));
+    }
+
+    private void DestroyThis() => Destroy(gameObject);
+    
+    private IEnumerator StartTimer(float duration, Action methodToCall)
+    {
+        float timer = duration;
+        while (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        
+            yield return null;
+        }
+        methodToCall();
+    }
+}
